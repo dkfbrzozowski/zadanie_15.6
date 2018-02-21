558ff55f0993ac0812daa9b67b2b10c59d808b2b
Dawid Brzozowski <d.brzozowski@wp.pl>
2018-02-21 22:44:54 +0100

    Add marks

--- script-compiled.js
+++ script-compiled.js
@@ -5,104 +5,141 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Stopwatch = function () {
-				function Stopwatch(display) {
-								_classCallCheck(this, Stopwatch);
+    function Stopwatch(display, results) {
+        _classCallCheck(this, Stopwatch);
 
-								this.running = false;
-								this.display = display;
-								this.reset();
-								this.print(this.times);
-				}
+        this.running = false;
+        this.display = display;
+        this.results = results;
+        this.reset();
+        this.print(this.times);
+        this.marks = [];
+    }
 
-				_createClass(Stopwatch, [{
-								key: 'reset',
-								value: function reset() {
-												this.times = {
-																minutes: 0,
-																seconds: 0,
-																miliseconds: 0
-												};
-								}
-				}, {
-								key: 'print',
-								value: function print() {
-												this.display.innerText = this.format(this.times);
-								}
-				}, {
-								key: 'format',
-								value: function format(times) {
-												return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
-								}
-				}, {
-								key: 'start',
-								value: function start() {
-												var _this = this;
+    _createClass(Stopwatch, [{
+        key: 'reset',
+        value: function reset() {
+            this.times = {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            };
+        }
+    }, {
+        key: 'print',
+        value: function print() {
+            this.display.innerText = this.format(this.times);
+        }
+    }, {
+        key: 'format',
+        value: function format(times) {
+            return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds)) + ' ';
+        }
+    }, {
+        key: 'start',
+        value: function start() {
+            var _this = this;
 
-												if (!this.running) {
-																this.running = true;
-																this.watch = setInterval(function () {
-																				return _this.step();
-																}, 10);
-												}
-								}
-				}, {
-								key: 'step',
-								value: function step() {
-												if (!this.running) return;
-												this.calculate();
-												this.print();
-								}
-				}, {
-								key: 'calculate',
-								value: function calculate() {
-												this.times.miliseconds += 1;
-												if (this.times.miliseconds >= 100) {
-																this.times.seconds += 1;
-																this.times.miliseconds = 0;
-												}
-												if (this.times.seconds >= 60) {
-																this.times.minutes += 1;
-																this.times.seconds = 0;
-												}
-								}
-				}, {
-								key: 'stop',
-								value: function stop() {
-												this.running = false;
-												clearInterval(this.watch);
-								}
-				}, {
-								key: 'clear',
-								value: function clear() {
-												this.reset();
-												this.print();
-								}
-				}]);
+            if (!this.running) {
+                this.running = true;
+                this.watch = setInterval(function () {
+                    return _this.step();
+                }, 10);
+            }
+        }
+    }, {
+        key: 'step',
+        value: function step() {
+            if (!this.running) return;
+            this.calculate();
+            this.print();
+        }
+    }, {
+        key: 'calculate',
+        value: function calculate() {
+            this.times.miliseconds += 1;
+            if (this.times.miliseconds >= 100) {
+                this.times.seconds += 1;
+                this.times.miliseconds = 0;
+            }
+            if (this.times.seconds >= 60) {
+                this.times.minutes += 1;
+                this.times.seconds = 0;
+            }
+        }
+    }, {
+        key: 'stop',
+        value: function stop() {
+            this.running = false;
+            clearInterval(this.watch);
+        }
+    }, {
+        key: 'clear',
+        value: function clear() {
+            this.reset();
+            this.print();
+            // this.stop();
+        }
+    }, {
+        key: 'mark',
+        value: function mark() {
+            this.marks.push(this.format(this.times));
+            this.printMarks();
+        }
+    }, {
+        key: 'printMarks',
+        value: function printMarks() {
+            this.results.innerText = '';
+            var frag = document.createDocumentFragment();
+            this.marks.forEach(function (mark) {
+                var element = document.createElement('li');
+                element.innerHTML = mark;
+                frag.appendChild(element);
+            });
+            this.results.appendChild(frag);
+        }
+    }, {
+        key: 'clearMarkers',
+        value: function clearMarkers() {
+            this.marks = [];
+            this.printMarks();
+        }
+    }]);
 
-				return Stopwatch;
+    return Stopwatch;
 }();
 
 function pad0(value) {
-				var result = value.toString();
-				if (result.length < 2) {
-								result = '0' + result;
-				}
-				return result;
+    var result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
 }
 
-var stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+var stopwatch = new Stopwatch(document.querySelector('.stopwatch'), document.querySelector('.results'));
 
 var startButton = document.getElementById('start');
 startButton.addEventListener('click', function () {
-				return stopwatch.start();
+    return stopwatch.start();
 });
 
 var stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
-				return stopwatch.stop();
+    return stopwatch.stop();
 });
 
 var clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', function () {
-				return stopwatch.clear();
+    return stopwatch.clear();
+});
+
+var markButton = document.getElementById('mark');
+markButton.addEventListener('click', function () {
+    return stopwatch.mark();
+});
+
+var clearMarkersButton = document.getElementById('clearMarkers');
+clearMarkersButton.addEventListener('click', function () {
+    return stopwatch.clearMarkers();
 });
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 class Stopwatch {
-    constructor(display) {
+    constructor(display, results) {
         this.running = false;
         this.display = display;
+        this.results = results;
         this.reset();
         this.print(this.times);
+        this.marks = [];
     }
-
     reset() {
         this.times = {
             minutes: 0,
@@ -13,68 +14,86 @@ class Stopwatch {
             miliseconds: 0
         };
     }
-
     print() {
         this.display.innerText = this.format(this.times);
-	}
-
-	format(times) {
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-	}
-
-	start() {
-	    if (!this.running) {
-	        this.running = true;
-	        this.watch = setInterval(() => this.step(), 10);
-	    }
-	}
-
-	step() {
-	    if (!this.running) return;
-	    this.calculate();
-	    this.print();
-	}
-
-	calculate() {
-	    this.times.miliseconds += 1;
-	    if (this.times.miliseconds >= 100) {
-	        this.times.seconds += 1;
-	        this.times.miliseconds = 0;
-	    }
-	    if (this.times.seconds >= 60) {
-	        this.times.minutes += 1;
-	        this.times.seconds = 0;
-	    }
-	}
-
-	stop() {
-	    this.running = false;
-	    clearInterval(this.watch);
-	}
-
-	clear() {
-		this.reset();
-		this.print();
-	}
+    }
+    format(times) {
+        return(`${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))} `);
+    }
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+    clear() {
+        this.reset();
+        this.print();
+        // this.stop();
+    }
+    mark() {
+        this.marks.push(this.format(this.times));
+        this.printMarks();
+    }
+    printMarks() {
+        this.results.innerText = '';
+        let frag = document.createDocumentFragment();
+        this.marks.forEach((mark) => {
+            let element = document.createElement('li');
+            element.innerHTML = mark;
+            frag.appendChild(element);
+        });
+        this.results.appendChild(frag);
+    }
+    clearMarkers() {
+        this.marks = [];
+        this.printMarks();
+    }
+    
 }
 
 function pad0(value) {
     let result = value.toString();
     if (result.length < 2) {
-        result = '0' + result;
+        result = `0${result}`;
     }
     return result;
 }
 
-const stopwatch = new Stopwatch(
-document.querySelector('.stopwatch'));
 
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'), document.querySelector('.results'));
 
-var startButton = document.getElementById('start');
+let startButton = document.getElementById('start');
 startButton.addEventListener('click', () => stopwatch.start());
 
-var stopButton = document.getElementById('stop');
+let stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', () => stopwatch.stop());
 
-var clearButton = document.getElementById('clear');
+let clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', () => stopwatch.clear());
+
+let markButton = document.getElementById('mark');
+markButton.addEventListener('click', () => stopwatch.mark());
+
+let clearMarkersButton = document.getElementById('clearMarkers');
+clearMarkersButton.addEventListener('click', () => stopwatch.clearMarkers());
